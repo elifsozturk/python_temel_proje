abdfbf026def17942bb77afcede455cabe8a6e42
Elif Sıla Öztürk <103839449+elifsozturk@users.noreply.github.com>
2022-09-20 19:33:01 +0300

    Add files via upload

---
+++ reversed_list_function.py
@@ -0,0 +1,8 @@
+list = [[1, 2], [3, 4], [5, 6, 7]]
+
+def reversed_list(list, reverse_list):
+    reverse_list = [i[::-1] for i in list]
+    return reverse_list[::-1]
+
+revers_list = []
+print(reversed_list(list, revers_list))
