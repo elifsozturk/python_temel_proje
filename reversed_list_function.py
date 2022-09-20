9ad35160cabcccc8f4a75995369d6bac54cb9900
Elif Sıla Öztürk <103839449+elifsozturk@users.noreply.github.com>
2022-09-20 19:35:51 +0300

    son düzenlemeler yapıldı

--- reversed_list_function.py
+++ reversed_list_function.py
@@ -1,8 +1,7 @@
-list = [[1, 2], [3, 4], [5, 6, 7]]
-
 def reversed_list(list, reverse_list):
     reverse_list = [i[::-1] for i in list]
     return reverse_list[::-1]
 
+list = [[1, 2], [3, 4], [5, 6, 7]]
 revers_list = []
 print(reversed_list(list, revers_list))
